da87604cf0da3baa2d293284e9164d7cbd215e1e
elmerRonyQH <33467835+elmerRonyQH@users.noreply.github.com>
2017-11-23 14:37:19 -0400

    Add files via upload

---
+++ Archivo1.java
@@ -0,0 +1,37 @@
+
+
+package archivo1;
+
+
+import java.util.*;
+
+public class Archivo1 
+{
+
+
+    public static void main(String[] args) 
+    {
+        Alumno obj = new Alumno();
+        //obj.mostrar();
+        
+        
+        String bus;
+        System.out.print("BUSCAR NOMBRE : ");
+        Scanner in = new Scanner(System.in);
+        bus = in.nextLine();
+        if(bus.trim().equals(""))
+        {
+        	obj.mostrar();
+        }
+        else
+        {
+        	obj.Buscar(bus);
+        }
+        Calificacion obj2 = new Calificacion();
+        obj2.MostrarCalificaciones();
+        
+        
+        
+    }
+    
+}
